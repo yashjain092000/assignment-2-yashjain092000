7fe7bfb21a88fbba18af456cf920431a7f48304f
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-03-28 20:02:30 -0500

    main class complete of problem2

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,28 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size ");
+        int size = sc.nextInt();
+        MyBinarySearchTree b=new MyBinarySearchTree();
+        for (int i = 0; i <size ; i++) {
+            b.insert(sc.nextInt());
+
+        }
+        System.out.println("Pre Order Print:----");
+        b.print();
+        System.out.println("\nPost Order Print:----");
+        b.printPostorder();
+    }
+
+
 }
