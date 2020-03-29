111a3cddc2fa439a3f61a1b6eb44a1a081db7c28
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-03-28 19:13:23 -0500

    main class complete

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,7 +7,24 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
+
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        MyBinarySearchTree b= new MyBinarySearchTree();
+        System.out.println("Enter size");
+        int size=sc.nextInt();
+        for (int i = 0; i <size ; i++) {
+            b.insert(sc.nextInt());
+        }
+        System.out.println("Printing:");
+        b.print();
+    }
+
 
 
 
